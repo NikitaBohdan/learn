dda58f9d8190697b5760505ddd8102352d66b471
Nikita <nikitabohdan16@gmail.com>
2025-02-06 22:25:09 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+"use strict";
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// if (4 == 9) {
+//     console.log("Ok!");
+// } else {
+//     console.log("Error!");
+// }
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оценните его?", ""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt("На сколько оценните его?", "");
+// const num = 50;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// if (num < 49) {
+//     console.log("Error!");
+// } else if (num > 100) {
+//     console.log("Many");
+// } else {
+//     console.log("Ok!");
+// }
+//
+// (num === 50) ? console.log("Ok!") : console.log("Error!");
 
-console.log(personalMovieDB)
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log("Неверно");
+        break;
+    case 100 :
+        console.log("Неверно");
+        break;
+    case 50:
+        console.log("В точку!")
+        break;
+    default:
+        console.log("Не в этот раз")
+        break;
+}
+
+
